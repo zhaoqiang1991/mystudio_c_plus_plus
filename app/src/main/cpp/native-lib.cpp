0cda2bb810098e937287cb476eab82a17439a940
l <112324121@qq.com>
2023-05-26 10:41:56 +0800

    1.init c++ project

---
+++ app/src/main/cpp/native-lib.cpp
@@ -0,0 +1,13 @@
+#include <jni.h>
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+extern "C" JNIEXPORT jstring JNICALL
+Java_com_example_myapplication_MainActivity_stringFromJNI(
+        JNIEnv *env,
+        jobject /* this */) {
+    string hello = "Hello from C++";
+    return env->NewStringUTF(hello.c_str());
+}

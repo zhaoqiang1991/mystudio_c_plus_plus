fa2e3689323df21199b5642b4544f0d0366a0167
l <112324121@qq.com>
2023-05-27 16:47:23 +0800

    1.创建头文件

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -3,14 +3,8 @@
 #include <iostream>
 #include <pthread.h>
 #include <android/log.h>
+#include "util.h"
 
-
-#define TAG "FFMPEG" // 这个是自定义的LOG的标识
-#define LOGD(...) __android_log_print(ANDROID_LOG_DEBUG,TAG ,__VA_ARGS__) // 定义LOGD类型
-#define LOGI(...) __android_log_print(ANDROID_LOG_INFO,TAG ,__VA_ARGS__) // 定义LOGI类型
-#define LOGW(...) __android_log_print(ANDROID_LOG_WARN,TAG ,__VA_ARGS__) // 定义LOGW类型
-#define LOGE(...) __android_log_print(ANDROID_LOG_ERROR,TAG ,__VA_ARGS__) // 定义LOGE类型
-#define LOGF(...) __android_log_print(ANDROID_LOG_FATAL,TAG ,__VA_ARGS__) // 定义LOGF类型
 
 using namespace std;
 
@@ -33,9 +27,9 @@ extern "C"
 JNIEXPORT jstring JNICALL
 Java_com_example_myapplication_MainActivity_shareSchoolInfo(JNIEnv *env, jobject thiz, jstring name,
                                                             jint age) {
-    const jchar* jName = env->GetStringChars(name,0);
+    const jchar *jName = env->GetStringChars(name, 0);
 
-   // LOGD("########## i = %d", jName);
+    // LOGD("########## i = %d", jName);
     LOGD("########## i = %d", age);
     //__android_log_print(ANDROID_LOG_DEBUG,TAG ,"");
     return env->NewStringUTF("llll");;

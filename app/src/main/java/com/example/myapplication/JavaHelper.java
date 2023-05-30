1944bcc698034ef45dafe387e6d92a9e759c1126
l <112324121@qq.com>
2023-05-30 16:00:14 +0800

    1.定义宏开关

--- app/src/main/java/com/example/myapplication/JavaHelper.java
+++ app/src/main/java/com/example/myapplication/JavaHelper.java
@@ -3,6 +3,10 @@ package com.example.myapplication;
 import android.content.Context;
 import android.util.Log;
 
+/**
+ * 这里面报错的 native方法需要在CMakelist中把ENABLE_DYNAMIC_JNI_ONLOAD
+ * 这个宏开关打开就可以了
+ */
 public class JavaHelper {
     public native void getNativeAddress();
 
